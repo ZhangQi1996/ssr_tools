c6db34f8c20dd5977beaf479ce98a2bafdc7561a
david <358929931@qq.com>
2020-02-29 17:42:00 +0800

    更新ssr下载为tgz格式

--- ssrmu.sh
+++ ssrmu.sh
@@ -858,17 +858,17 @@ Download_SSR(){
   cd $cur_path || exit 1
 	[[ -d $ssr_folder ]] && echo -e "ssr的安装目录已经存在，请卸载先前的ssr或者将该目录删除(rm -rf $ssr_folder)后在重新安装" && exit 1
 	# wget -N --no-check-certificate "https://github.com/ToyoDAdoubiBackup/shadowsocksr/archive/manyuser.zip"
-	wget -N --no-check-certificate https://gitee.com/ChiZhung/ssr_tools/raw/master/shadowsocksr-manyuser.zip
+	wget -N --no-check-certificate https://gitee.com/ChiZhung/ssr_tools/raw/master/shadowsocksr-manyuser.tgz
 	#git config --global http.sslVerify false
 	#env GIT_SSL_NO_VERIFY=true git clone -b manyuser https://github.com/ToyoDAdoubiBackup/shadowsocksr.git
 	#[[ ! -e ${ssr_folder} ]] && echo -e "${Error} ShadowsocksR服务端 下载失败 !" && exit 1
-	[[ ! -e "shadowsocksr-manyuser.zip" ]] && echo -e "${Error} ShadowsocksR服务端 压缩包 下载失败 !" && rm -rf shadowsocksr-manyuser.zip && exit 1
+	[[ ! -e "shadowsocksr-manyuser.tgz" ]] && echo -e "${Error} ShadowsocksR服务端 压缩包 下载失败 !" && rm -rf shadowsocksr-manyuser.tgz && exit 1
 	[[ -e /tmp/shadowsocksr-manyuser/ ]] && rf -rf /tmp/shadowsocksr-manyuser/
-	unzip "shadowsocksr-manyuser.zip" -d /tmp
-	[[ ! -e /tmp/shadowsocksr-manyuser ]] && echo -e "${Error} ShadowsocksR服务端 解压失败 !" && rm -rf shadowsocksr-manyuser.zip && exit 1
+	tar -zxf shadowsocksr-manyuser.tgz -C /tmp
+	[[ ! -e /tmp/shadowsocksr-manyuser ]] && echo -e "${Error} ShadowsocksR服务端 解压失败 !" && rm -rf shadowsocksr-manyuser.tgz && exit 1
 	mv -f /tmp/shadowsocksr-manyuser $ssr_folder
-	[[ ! -e $ssr_folder ]] && echo -e "${Error} ShadowsocksR服务端 重命名失败 !" && rm -rf shadowsocksr-manyuser.zip && rm -rf "/tmp/shadowsocksr-manyuser/" && exit 1
-	rm -rf shadowsocksr-manyuser.zip
+	[[ ! -e $ssr_folder ]] && echo -e "${Error} ShadowsocksR服务端 重命名失败 !" && rm -rf shadowsocksr-manyuser.tgz && rm -rf "/tmp/shadowsocksr-manyuser/" && exit 1
+	rm -rf shadowsocksr-manyuser.tgz
 	cd $ssr_folder || exit 1
 	cp "${ssr_folder}/config.json" "${config_user_file}"
 	cp "${ssr_folder}/mysql.json" "${ssr_folder}/usermysql.json"
